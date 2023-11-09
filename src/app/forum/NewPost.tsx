157171e32f6aa51ffeca9ef7f173e31e3eb9d157
Ntholi Nkhatho <ntholi@live.com>
2023-11-09 13:32:49 +0200

    setup firebase admin auth

--- src/app/forum/NewPost.tsx
+++ src/app/forum/NewPost.tsx
@@ -6,14 +6,17 @@ import { useState } from 'react';
 
 export default function NewPost() {
   const [content, setContent] = useState('');
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
 
   const handleSubmit = async () => {
-    let userId;
-    createPost({
-      content,
-      userId: session?.user?.id,
-    });
+    if (!session?.user) return;
+
+    if (content && content.trim().length > 0) {
+      createPost({
+        content,
+        userId: session?.user?.id,
+      });
+    }
   };
 
   return (
@@ -21,6 +24,8 @@ export default function NewPost() {
       <Textarea
         className=''
         variant='bordered'
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
         label='New Topic'
         placeholder='What do you want to discuss about?'
       />
