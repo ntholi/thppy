1994ee0f4ab0209e7f9acf7798793743c0199cb5
Ntholi Nkhatho <ntholi@live.com>
2023-11-23 12:07:30 +0200

    setup FirestoreAdapter

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,97 +1,25 @@
-import { Account, AuthOptions, User } from 'next-auth';
+import { AuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import FacebookProvider from 'next-auth/providers/facebook';
-import { auth } from '@/lib/firebase';
-import { adminAuth } from '@/lib/firebase-admin';
-import {
-  GoogleAuthProvider,
-  signInWithCredential,
-  signInWithEmailAndPassword,
-} from 'firebase/auth';
+import { ServiceAccount, cert } from 'firebase-admin/app';
+import { FirestoreAdapter } from '@auth/firebase-adapter';
+
+export const googleCredentials: ServiceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    : undefined,
+};
 
 export const authOptions = {
   secret: process.env.SECRET,
-  session: {
-    strategy: 'jwt',
-  },
-  // debug: process.env.NODE_ENV === 'development',
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
-
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'text' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials, req) {
-        if (!credentials?.email || !credentials.password) {
-          throw new Error('Enter email and password');
-        }
-        const { user } = await signInWithEmailAndPassword(
-          auth,
-          credentials.email,
-          credentials.password,
-        );
-
-        if (!user) {
-          throw new Error('User not found');
-        }
-        return {
-          id: user.uid,
-          email: user.email,
-          name: user.displayName,
-          image: user.photoURL,
-        } as any;
-      },
-    }),
   ],
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      const isAllowedToSignIn = true;
-      if (isAllowedToSignIn) {
-        if (account?.provider === 'google') {
-          const credential = GoogleAuthProvider.credential(account.id_token);
-          await signInWithCredential(auth, credential);
-        }
-        return true;
-      } else {
-        return false;
-      }
-    },
-
-    async jwt({ token, account, profile, user }) {
-      if (user && account) {
-        const uid = await getFirebaseUserId(user, account);
-        if (uid) {
-          token.id = uid;
-        }
-      }
-      return token;
-    },
-    async session({ session, token, user }) {
-      if (session.user) {
-        session.user.id = token.id;
-        session.user.role = token.role;
-      }
-      return session;
-    },
-  },
+  adapter: FirestoreAdapter({
+    credential: cert(googleCredentials),
+  }),
 } as AuthOptions;
-
-async function getFirebaseUserId(user: User, account: Account | null) {
-  let firebaseUser;
-  if (account?.provider === 'google') {
-    firebaseUser = await adminAuth.getUserByProviderUid(
-      'google.com',
-      account.providerAccountId,
-    );
-  } else {
-    firebaseUser = await adminAuth.getUserByEmail(user.email || '');
-  }
-  return firebaseUser.uid;
-}
