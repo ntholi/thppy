786801644c6b57b049280b1f90acad3729c3dc65
Ntholi Nkhatho <ntholi@live.com>
2023-11-23 14:39:54 +0200

    Update main container padding in ForumPage component

--- src/app/forum/page.tsx
+++ src/app/forum/page.tsx
@@ -5,7 +5,7 @@ import PostList from './PostList';
 export default function ForumPage() {
   return (
     <div className='min-h-screen bg-gray-100'>
-      <main className='mt-24 h-60 rounded-sm  md:mx-auto md:w-[60vw]'>
+      <main className='h-60 rounded-sm pt-16  md:mx-auto md:w-[60vw]'>
         <section className='pt-5'>
           <NewPost />
         </section>
